05c61b3b32269c830cf34a748078f46ee1f238b0
mwylerCH <61991286+mwylerCH@users.noreply.github.com>
2023-11-20 16:44:27 +0100

    Update InfSplitter.ps1

--- InfSplitter.ps1
+++ InfSplitter.ps1
@@ -1,7 +1,15 @@
-# PI.ps1
+# InfSplitter.ps1
 
 
-$PATH = ".\Documents\oneChicken.fa"
+# Splits up influenza fasta with a single file for each segment
+# Michele Wyler, IVI Mittelhäusern, 20.12.2023
+
+
+# read in file as argument
+Param(
+  [parameter(mandatory=$true)][string]$PATH
+)
+
 
 # read in file
 $Original = Get-Content -Path $PATH
@@ -17,58 +25,46 @@ $CHIAVE
 $SEQ
 
 ForEach ($Line in $Original) {
-
-	if($Line -like ">*"){
-		$CHIAVE = $Line		
-	} elseif ($Line -like "*"){
-		# test if key exist already
-		if ($Fasta.Keys -contains $CHIAVE) {
-			$SEQ = $Fasta[$CHIAVE] + $LINE
-			# remove first...
-			$Fasta.Remove($CHIAVE)
-			# ...and add longer sequence
-			$Fasta.Add($CHIAVE, $SEQ)
-		} else {
-			# if it's new
-			$Fasta.Add($CHIAVE, $Line)
-		}
-
-
-
+	if ($Line -notlike ''){
+			if($Line -like ">*"){
+				$CHIAVE = $Line		
+			} elseif ($Line -like "*"){
+				# test if key exist already
+				if ($Fasta.Keys -contains $CHIAVE) {
+					$SEQ = $Fasta[$CHIAVE] + $LINE
+					# remove first...
+					$Fasta.Remove($CHIAVE)
+					# ...and add longer sequence
+					$Fasta.Add($CHIAVE, $SEQ)
+				} else {
+					# if it's new
+					$Fasta.Add($CHIAVE, $Line)
+				}
+			}
 	}
-    # Process each line here
 }
-
 
 ### Split up -----------------------------
 
 $FastaHeads = $Fasta.Keys
 
-#for (($NR = 1); $NR -lt 9; $NR++) {
-for (($NR = 1); $NR -lt 2; $NR++) {
+for (($NR = 1); $NR -lt 9; $NR++) {
 	# open file for each segment
-	"ecco $NR"
-	ForEach ($head in $Original) {
-		# test if key contains the number
-		#if ($head -like "*_$NR_*" -or $head -like "*\|$NR\|*"){
-		#if ($head -like ".*_$NR_.*" -or $head -like ".*\|$NR\|.*"){
-$head = ">|PB2|Huhn_Switzerland1|Huhn|Fehraltorf"
-$head
-		if ($head -like ".*\|$NR\|.*"){
-			$head
+	$OUTfile = $PATH.replace('.fasta','.fa')
+	$OUTfile = $OUTfile.replace('.fa',"_segment$NR.fa")
+
+	# remove file (if already present)
+	$fileExists = Test-Path -Path $OUTfile
+	if ($fileExists) {
+		Remove-Item -Path $OUTfile
+	}
+
+	# loop through hash (and write out)	
+	ForEach ($head in $Fasta.Keys) {
+		# test if key contains the seg number
+		if (($head -match ".*_${NR}_.*") -or ($head -match ".*\|$NR\|.*")){
+			Out-File -Append -FilePath $OUTfile -InputObject $head
+			Out-File -Append -FilePath $OUTfile -InputObject $Fasta[$head]
 		}
 	}
 }
-
-
-
-
-# $FastaHeads
-
-
-
-
-
-
-
-
